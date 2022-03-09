8e020fd60b91f665be2be25af760765a0ba503af
Nahum Casco <nahumcasco.v@gmail.com>
2022-03-08 21:00:24 -0500

    show data on admin page

---
+++ pages/api/datalog/comments_amount.js
@@ -0,0 +1,9 @@
+import {collection,getDocs,where,query} from 'firebase/firestore'
+import { database } from '../../../database'
+
+export default async function list(req,res){
+    const commentsCollection = collection(database, 'comments')
+    const snapshot = await getDocs(commentsCollection)
+    // console.log(docs.size)
+    return res.json(snapshot.size)
+}
