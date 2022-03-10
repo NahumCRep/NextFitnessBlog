67f867f7908ed1b95ab18442c33df41312f05c30
Nahum Casco <nahumcasco.v@gmail.com>
2022-03-10 09:21:24 -0500

    adjust auto fit row

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,14 +23,16 @@ module.exports = {
       gridTemplateColumns: {
         'auto-fit': 'repeat(auto-fit, minmax(300px, 1fr))',
         'auto-fill': 'repeat(auto-fill, minmax(300px, 1fr))',
+        'auto-fit-450': 'repeat(auto-fit, minmax(450px, 1fr))',
       },
       gridTemplateRows: {
-        'auto-fit': 'repeat(auto-fit, minmax(300px, 1fr))',
+        'auto-fit': 'repeat(auto-fit, minmax(0, 200px))',
         'auto-fill': 'repeat(auto-fill, minmax(300px, 1fr))',
       },
     },
   },
   plugins: [
+    require('@tailwindcss/typography'),
     require('@tailwindcss/line-clamp'),
   ],
 }
