692ffcbde6bb925cf0598f84c7d99350712be759
Nahum Casco <nahumcasco.v@gmail.com>
2022-03-10 09:18:45 -0500

    setDoc to update document after edit

--- pages/api/posts/[id].js
+++ pages/api/posts/[id].js
@@ -1,4 +1,4 @@
-import { doc, getDoc, deleteDoc } from "firebase/firestore"
+import { doc, getDoc, deleteDoc, setDoc } from "firebase/firestore"
 import { database } from "../../../database"
 
 export default async function getPost(req,res){
@@ -11,6 +11,13 @@ export default async function getPost(req,res){
         console.log(req.query.id)
         await deleteDoc(doc(database,"posts",req.query.id))
         return res.status(200).json({message:'post deleted'}) 
+    }
+
+    if(req.method == 'PUT'){
+        // console.log(req.query.id)
+        // console.log(req.body)
+        await setDoc(doc(database,"posts",req.query.id), req.body)
+        return res.status(200).json({message:'post edited'}) 
     }
 
     return res.status(200).json({message:'operacion no disponible'})
