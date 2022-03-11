94c01c67049b310bb10216b9c2de575c592e4e80
Nahum Casco <nahumcasco.v@gmail.com>
2022-03-11 16:05:44 -0500

    search all and highlights

--- pages/admin/posts/index.jsx
+++ pages/admin/posts/index.jsx
@@ -11,15 +11,18 @@ import AdminPostsList from '../../../components/AdminPostsList'
 export async function getServerSideProps(context) {
     const secure = context.req.connection.encrypted
     let postsRes
-    if(context.query.name){
+    if (context.query.name) {
         const url = `${secure ? "https" : "http"}://${context.req.headers.host}/api/posts/search?name=${context.query.name}`
         postsRes = await axios.get(url)
-    }else{
+    }else if(context.query.highlights){
+        const postUrl = `${secure ? "https" : "http"}://${context.req.headers.host}/api/posts/highlights`
+        postsRes = await axios.get(postUrl)
+    }else {
         const url = `${secure ? "https" : "http"}://${context.req.headers.host}/api/posts`
         postsRes = await axios.get(url)
     }
-    
-   
+
+
     return {
         props: {
             posts: postsRes.data
@@ -46,12 +49,23 @@ const Posts = ({ posts }) => {
         }
     }
 
+    const getAllPosts = () => {
+        router.push('/admin/posts')
+    }
+
+    const getAllHighlights = () => {
+        router.push('/admin/posts?highlights=true')
+    }
+
+
     return (
         <AdminPage>
             <section className='h-auto w-full'>
                 <div className='flex py-5 px-9 justify-between items-center flex-col md:flex-row'>
                     <h1 className='font-faudiowide text-2xl'>Posts</h1>
-                    <div className='flex justify-center items-center gap-9 flex-col md:flex-row w-full md:w-auto'>
+                    <div className='flex justify-center items-center gap-5 md:gap-9 flex-col md:flex-row w-full md:w-auto mt-5 md:mt-0'>
+                        <button onClick={() => getAllPosts()} className='w-full  h-10 rounded-lg md:w-28 font-fgrotesque text-xl font-bold bg-slate-100'>All</button>
+                        <button onClick={() => getAllHighlights()} className='w-full h-10 rounded-lg  md:w-28 font-fgrotesque text-xl font-bold bg-slate-100'>Highlights</button>
                         <div className='flex gap-2 mt-3 md:mt-0'>
                             <input ref={searchRef} type='text' className='w-[300px] text-center rounded-md px-2 outline-none border-none shadow-inner shadow-slate-500' />
                             <button onClick={() => searchPost()} className='h-10 p-2 rounded-md flex justify-center items-center bg-white shadow-inner shadow-slate-500'><FaSearch size={20} /></button>
