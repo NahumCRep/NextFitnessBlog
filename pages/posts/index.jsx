2723f33289184561f7c439afe672b94b90d5962e
Nahum Casco <nahumcasco.v@gmail.com>
2022-03-10 22:05:08 -0500

    search all and search highlights

--- pages/posts/index.jsx
+++ pages/posts/index.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import {useRef} from 'react'
+import {useRef, useState} from 'react'
 import { FaSearch } from 'react-icons/fa'
 import RectanglePostsList from '../../components/RectanglePostsList'
 import Loader from '../../components/Loader'
@@ -15,13 +15,14 @@ export async function getServerSideProps(context){
     }else if(context.query.category){
         const postUrl = `${secure ? "https" : "http"}://${context.req.headers.host}/api/posts/search?category=${context.query.category}`
         postsRes = await axios.get(postUrl)
+    }else if(context.query.highlights){
+        const postUrl = `${secure ? "https" : "http"}://${context.req.headers.host}/api/posts/highlights`
+        postsRes = await axios.get(postUrl)
     }else{
         const postUrl = `${secure ? "https" : "http"}://${context.req.headers.host}/api/posts`
         postsRes = await axios.get(postUrl)
     }
 
-    
-    // console.log(postsRes.data)
     const categoryUrl = `${secure ? "https" : "http"}://${context.req.headers.host}/api/categories`
     const categoriesRes = await axios.get(categoryUrl)
  
@@ -47,11 +48,21 @@ const RegularPosts = ({posts, categories}) => {
         }
     }
 
+    const getAllPosts = () =>{
+        router.push('/posts')
+    }
+
+    const getAllHighlights = () =>{
+        router.push('/posts?highlights=true')
+    }
+
     return (
         <section className='pt-[6rem] px-6 h-full min-h-screen flex flex-col items-center'>
             <div className='w-full flex py-5 justify-between items-center flex-col md:flex-row'>
                 <h1 className='font-faudiowide text-2xl'>Posts</h1>
-                <div className='flex justify-center items-center gap-9 flex-col md:flex-row w-full md:w-auto'>
+                <div className='flex justify-center items-center gap-9 flex-col md:flex-row w-full md:w-auto mt-3 md:mt-0'>
+                    <button onClick={()=>getAllPosts()} className='w-full md:w-28 font-fgrotesque text-xl font-bold bg-slate-100'>Search All</button>
+                    <button onClick={()=>getAllHighlights()} className='w-full md:w-28 font-fgrotesque text-xl font-bold bg-slate-100'>Highlights</button>
                     <div className='flex gap-2 mt-3 md:mt-0'>
                         <input ref={searchRef} type='text' className='w-[300px] text-center rounded-md px-2 outline-none border-none shadow-inner shadow-slate-500' />
                         <button onClick={() => searchPost()} className='h-10 p-2 rounded-md flex justify-center items-center bg-white shadow-inner shadow-slate-500'><FaSearch size={20} /></button>
