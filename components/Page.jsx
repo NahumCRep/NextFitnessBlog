764178f6e8948ef95a2239c1e08cf8cabc7b198e
Nahum Casco <nahumcasco.v@gmail.com>
2022-03-10 23:36:08 -0500

    footer

--- components/Page.jsx
+++ components/Page.jsx
@@ -1,23 +1,33 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Seo from './Seo'
+import Link from 'next/link'
 
-const Page = ({children}) => {
+const Page = ({ children }) => {
     return (
-        <div className='h-screen bg-slate-300 flex flex-col justify-between '>
+        <div className='min-h-screen h-auto bg-slate-300 flex flex-col justify-between '>
             <Seo />
             <Navbar />
             <main className='bg-slate-200 h-auto'>
                 {children}
             </main>
-            <footer className="bg-black p-10 text-white">
-                <ul className='flex'>
-                    <li>Link 1</li>
-                    <li>Link 1</li>
-                    <li>Link 1</li>
-                    <li>Link 1</li>
-                    <li>Link 1</li>
-                </ul>
+            <footer className="w-full h-60 md:h-52 bg-black p-10 text-white">
+                <div className='flex gap-9'>
+                    <ul className='flex flex-col font-fgrotesque'>
+                        <li className='text-xl font-bold'>Navigation</li>
+                        <li><Link href='/'>Home</Link></li>
+                        <li><Link href='/posts'>Posts</Link></li>
+                        <li><Link href='/about'>About</Link></li>
+                        <li><Link href='/posts?highlights=true'>Highlights</Link></li>
+                    </ul>
+                    <ul className='flex flex-col font-fgrotesque'>
+                        <li className='text-xl font-bold'>Contact</li>
+                        <li><Link href='/'>Facebook</Link></li>
+                        <li><Link href='/posts'>Instagram</Link></li>
+                        <li><Link href='/about'>Twitter</Link></li>
+                    </ul>
+                </div>
+                <p className='w-full text-center mt-8 md:mt-0 font-fgrotesque text-sm md:text-lg font-bold '>Tzuzul Bootcamp 2022 &copy; Nahum Casco</p>
             </footer>
         </div>
     )
