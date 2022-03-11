734b672a4954ea8d509684b644c7eac04e8b32dc
Nahum Casco <nahumcasco.v@gmail.com>
2022-03-11 18:57:05 -0500

    about page

---
+++ pages/about.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <section className='mt-20  w-full h-screen flex flex-col items-center justify-center gap-10'>
+        <article className='w-[60%]'>
+            <h1 className='font-faudiowide text-4xl' >Who's behind the blog ?</h1>
+            <p className='font-fgrotesque text-xl font-semibold mt-3 text-justify'>Aenean convallis scelerisque mauris nec sodales. Quisque sit amet velit vestibulum, pharetra neque et, vehicula lorem. Curabitur purus lorem, tempor at ultrices et, porta vitae nunc. Maecenas massa neque, rutrum eget egestas faucibus, pellentesque interdum erat. Morbi vitae urna eu enim consectetur lobortis in a arcu. Sed commodo mi vel auctor cursus. Nam ornare massa id nisi euismod eleifend. Ut eget orci nec orci tincidunt consequat vitae commodo orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam orci felis, dapibus eu tortor a, pretium aliquam velit. Aenean ut erat laoreet, tristique turpis eget, placerat odio. Pellentesque quis leo id massa ultricies eleifend.</p>
+        </article>
+        <article className='w-[60%]'>
+            <h1 className='font-faudiowide text-4xl'>What is the purpose of the blog ?</h1>
+            <p className='font-fgrotesque text-xl font-semibold mt-3 text-justify'>Aenean convallis scelerisque mauris nec sodales. Quisque sit amet velit vestibulum, pharetra neque et, vehicula lorem. Curabitur purus lorem, tempor at ultrices et, porta vitae nunc. Maecenas massa neque, rutrum eget egestas faucibus, pellentesque interdum erat. Morbi vitae urna eu enim consectetur lobortis in a arcu. Sed commodo mi vel auctor cursus. Nam ornare massa id nisi euismod eleifend. Ut eget orci nec orci tincidunt consequat vitae commodo orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam orci felis, dapibus eu tortor a, pretium aliquam velit. Aenean ut erat laoreet, tristique turpis eget, placerat odio. Pellentesque quis leo id massa ultricies eleifend.</p>
+        </article>
+    </section>
+  )
+}
+
+export default about
