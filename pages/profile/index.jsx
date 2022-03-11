c63869c9bd5a57ec2d9f1500462b089e40aad1e4
Nahum Casco <nahumcasco.v@gmail.com>
2022-03-11 16:05:12 -0500

    regular user profile

--- pages/profile/index.jsx
+++ pages/profile/index.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { useSession, signOut } from 'next-auth/react'
+import Image from 'next/image'
 
-const index = () => {
+const Profile = () => {
+  const { data: session } = useSession()
+  console.log(session)
   return (
-    <div>index</div>
+    <section className='w-full h-full min-h-screen mt-20'>
+      <div className='flex items-center bg-[#212121] p-4 gap-6'>
+        <div className='w-24 h-24 relative'>
+          <Image className='rounded-full' src={session.user.image} alt='profile picture' layout='fill' />
+        </div>
+        <div className='font-fgrotesque text-gray-50'>
+          <p className='text-base  md:text-xl font-semibold tracking-widest'>{session.user.name}</p>
+          <p className='text-base  md:text-xl font-semibold tracking-widest'>{session.user.email}</p>
+        </div>
+      </div>
+      <div>
+
+      </div>
+    </section>
   )
 }
 
-export default index
+export default Profile
