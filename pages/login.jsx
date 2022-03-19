e6d92d8e8919e5fed5ed389da7e38148b77c769c
Nahum Casco <nahumcasco.v@gmail.com>
2022-03-18 22:14:10 -0500

    width buttons changed

--- pages/login.jsx
+++ pages/login.jsx
@@ -15,18 +15,18 @@ export async function getServerSideProps(context) {
 export default function Login({ csrfToken }) {
   return (
     <section className='w-full h-screen flex flex-col justify-center items-center font-fgrotesque'>
-      <div className='relative w-[200px] h-[200px]'>
+      <div className='relative w-[250px] h-[250px]'>
           <Image src={'/login.svg'} alt='login image' layout='fill' />
       </div>
       <div>
         <h1 className='text-3xl font-bold mb-4'>Login with....</h1>
         <form className='flex justify-center mb-5' action='/api/auth/signin/github' method='POST'>
           <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
-          <button className='w-56 text-xl font-bold  transition-colors duration-700 ease-in-out bg-purple-700 p-3 rounded hover:bg-purple-500' type="submit"><AiFillGithub color='#ffffff' className='w-10 h-10 inline-block' /> GitHub</button>
+          <button className='w-72 text-xl font-bold  transition-colors duration-700 ease-in-out bg-purple-700 p-3 rounded hover:bg-purple-500' type="submit"><AiFillGithub color='#ffffff' className='w-10 h-10 inline-block' /> GitHub</button>
         </form>
         <form className='flex justify-center' action='/api/auth/signin/google' method='POST'>
           <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
-          <button className='w-56 text-xl font-bold  transition-colors duration-700 ease-in-out bg-purple-700 hover:bg-purple-500 p-3 rounded' type="submit"><AiFillGoogleCircle color='#ffffff' className='w-10 h-10 inline-block' /> Google</button>
+          <button className='w-72 text-xl font-bold  transition-colors duration-700 ease-in-out bg-purple-700 hover:bg-purple-500 p-3 rounded' type="submit"><AiFillGoogleCircle color='#ffffff' className='w-10 h-10 inline-block' /> Google</button>
         </form>
       </div>
 
